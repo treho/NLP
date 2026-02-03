08a390c4c0c922f9c6db88087028344a17dbbe64
treho <horst.treiblmaier@gmail.com>
2026-02-03 12:57:16 +0100

    "myCommit"

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+console.log("test");
+
+const arrItems =[];
+const newItem = "Coder Coder";
+
+function addItem(item) {
+    arrItems.push(item);
+    return arrItems;
+}
+
+addItem(newItem);
