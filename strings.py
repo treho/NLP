5943d3c48129d7676cb6822c1c09b6a5291cf92c
treho <horst.treiblmaier@gmail.com>
2026-02-10 12:16:24 +0100

    10 February 2026

--- strings.py
+++ strings.py
@@ -4,9 +4,15 @@ words = sentence.split()
 
 print(words)
 
-word_counts = {}
+word_counts = {} #this is a dictionary
+
+# for myWord in words:
+#     word_counts[myWord] = word_counts.get(myWord, 0) + 1 #returns 0 if myWord does not exist
 
 for myWord in words:
-    word_counts[myWord] = word_counts.get(myWord, 0) + 1
+    if myWord in word_counts:
+        word_counts[myWord] +=1;
+    else:
+        word_counts[myWord] = 1;
 
 print(word_counts)
---
+++ strings_a.py
@@ -0,0 +1,3 @@
+myDict = {"Max": "Hi", "Ann": "Ho", "Jack": "Hu"}
+print(myDict)
+print(myDict.get("Max", "Nix da"))
